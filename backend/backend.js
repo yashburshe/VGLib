2bd132cf969e10ae8f66d87dd00f9f5d460952d4
Ben Piperno <103087542+brpiperno@users.noreply.github.com>
2026-03-12 15:12:59 -0400

    updated backend to use new routes and check db initialization

--- backend/backend.js
+++ backend/backend.js
@@ -1,16 +1,25 @@
-import express from 'express';
+import express   from 'express';
+import { db }  from './db/mongo.js';
+import userRouter from "./routes/userRouter.js";
+import listRouter from "./routes/listRouter.js";
 
 try {
   process.loadEnvFile();
 } catch {
-  console.log("Unable to load env file. Not needed if this is in production!");
+  console.log("Unable to load env file. Ignore if in production!");
 }
-
 const PORT = process.env.PORT || 3000;
+
+if (!db) {
+  console.error("MongoDB database not initialized. Exiting.");
+  process.exit(1);
+}
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/user', userRouter);
+app.use('/api/list', listRouter);
 app.listen(PORT, () => {
   console.log("Server running in port ", PORT);
 });
