8c755ebc8c02ef67bb2f52b35efa7fa1b7f0ae55
Yash Burshe <bursheyash2@gmail.com>
2026-03-15 15:43:22 -0400

    Add services for videogames

---
+++ backend/services/gameService.js
@@ -0,0 +1,72 @@
+import { COLLECTIONS, db } from "../db/mongo";
+
+export async function getGameFromGameId(gameId) {
+  return await db.collection(COLLECTIONS.VIDEOGAMES).findOne({ id: gameId });
+}
+
+export async function getGames() {
+  return await db.collection(COLLECTIONS.VIDEOGAMES).find({});
+}
+
+export async function createGame(userId, game) {
+  const lastGameId = await db
+    .collection(COLLECTIONS.VIDEOGAMES)
+    .find()
+    .sort({ id: -1 })
+    .limit(1)
+    .toArray();
+
+  const nextGameId = lastGameId.length > 0 ? lastGameId[0].id + 1 : 1;
+
+  const gameCreated = await db.collection(COLLECTIONS.VIDEOGAMES).insertOne({
+    id: nextGameId,
+    createAt: new Date().toISOString(),
+    name: game.name,
+    platforms: game.platforms,
+    rating: game.rating,
+    slug: game.slug,
+    summary: game.summary,
+    userId: userId,
+  });
+
+  if (!gameCreated || !gameCreated.acknowledged) {
+    throw new Error("Failed to create game");
+  }
+
+  return nextGameId;
+}
+
+export async function deleteGame(gameId) {
+  const deleteResult = await db
+    .collection(COLLECTIONS.VIDEOGAMES)
+    .deleteOne({ id: gameId });
+
+  if (!deleteResult || deleteResult.deletedCount === 0) {
+    throw new Error("Game not found");
+  }
+  return;
+}
+
+export async function updateGame(game) {
+  const gameUpdated = await db.collection(COLLECTIONS.VIDEOGAMES).updateOne(
+    {
+      id: game.id,
+    },
+    {
+      $set: {
+        createAt: new Date().toISOString(),
+        name: game.name,
+        platforms: game.platforms,
+        rating: game.rating,
+        slug: game.slug,
+        summary: game.summary,
+      },
+    },
+  );
+
+  if (!gameUpdated || !gameUpdated.acknowledged) {
+    throw new Error("Failed to create game");
+  }
+
+  return;
+}
