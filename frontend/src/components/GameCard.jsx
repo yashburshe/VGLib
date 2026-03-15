176abbaddb6962554939b46602b82fc363aa9830
Yash Burshe <bursheyash2@gmail.com>
2026-03-15 18:06:38 -0400

    Update card style

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -3,17 +3,16 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router';
 
 export default function GameCard({ game }) {
-
     return (
-        <Link style={{ textDecoration: 'none' }} to={`/games/${game.id}`}>
-            <Card className='h-100 shadow-sm'>
-                <Card.Img style={{ objectFit: 'cover', height: '250px' }} variant="top" src={game.cover_url} />
-
-                <Card.Body className='p-3'>
+        <Card className='h-100 shadow-sm'>
+            <Card.Img style={{ objectFit: 'cover', height: '250px' }} variant="top" src={game.cover_url} />
+            <Card.Body className='p-3'>
+                <Link to={`/games/${game.id}`}>
                     <Card.Title className='fs-6 fw-bold mb-4 text-dark'>{game.name}</Card.Title>
-                    <Button size='sm' variant='outline-primary'>Add To List</Button>
-                </Card.Body>
-            </Card>
-        </Link>
+                </Link>
+                <Button size='sm' variant='outline-primary'>Add To List</Button>
+            </Card.Body>
+        </Card>
+
     )
 }
