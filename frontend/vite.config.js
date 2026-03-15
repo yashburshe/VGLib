9eb6c08723214a3842fdd7e9516e3546bfe8996a
Ben Piperno <103087542+brpiperno@users.noreply.github.com>
2026-03-15 09:41:09 -0400

    corrected proxy server logic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-      secure: false,
+  server: {
+    proxy: {
+      '/api': { 
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
