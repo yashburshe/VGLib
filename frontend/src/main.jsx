b18af67dfb53a0d2e7b5a4ef2a4528283607c8ce
Yash Burshe <bursheyash2@gmail.com>
2026-03-15 16:59:03 -0400

    Add bootstrap css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
 import LoginPage from './pages/login_page.jsx'
