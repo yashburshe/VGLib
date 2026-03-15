e1272d0c9df87ba15df284870d113d6828f4b078
Yash Burshe <bursheyash2@gmail.com>
2026-03-15 17:27:30 -0400

    Add navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,27 +1,23 @@
-import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar({ userID }) {
   return (
-    <header className="mb-auto">
-        <div>
-          <Link className="nav-link" to="/">
-            <h1 className="float-md-start mb-0">VGLib</h1>
-          </Link>
-          <nav className="nav nav-masthead justify-content-center float-md-end">
-            <Link className="nav-link" to="/">
-              Top
-            </Link>
-            <Link className="nav-link" to="/lists">
-              Lists
-            </Link>
-            <Link className="nav-link" to="/users">
-              Users
-            </Link>
-            <Link className="nav-link" to="/profile">
-              My Account
-            </Link>
-          </nav>
-        </div>
-      </header>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">VGLib</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/top">Top</Nav.Link>
+            <Nav.Link href="/lists">Lists</Nav.Link>
+            <Nav.Link href="/users">Users</Nav.Link>
+            <Nav.Link href="/profile">My Account</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
