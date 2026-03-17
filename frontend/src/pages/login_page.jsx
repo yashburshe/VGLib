534cbaf39aa19c1aa015cda5df9030a4a2987781
Ben Piperno <103087542+brpiperno@users.noreply.github.com>
2026-03-17 08:40:09 -0400

    added frontend fetching function with temp code, minor refactoring of profile page

--- frontend/src/pages/login_page.jsx
+++ frontend/src/pages/login_page.jsx
@@ -3,7 +3,6 @@ import { useNavigate} from 'react-router-dom';
 
 import LoginForm from "../components/LoginForm";
 import RegistrationForm from "../components/RegistrationForm";
-import NavBar from '../components/Nav';
 
 export default function LoginPage() {
     const [isLoginScreen, setIsLoginScreen] = useState(true);
