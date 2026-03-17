ff9da87b999cf87156a6ccd418cacc1a262df229
Ben Piperno <103087542+brpiperno@users.noreply.github.com>
2026-03-16 21:42:58 -0400

    removed nav bar

--- frontend/src/pages/login_page.jsx
+++ frontend/src/pages/login_page.jsx
@@ -10,7 +10,6 @@ export default function LoginPage() {
     const navigate = useNavigate();
     return (
         <>
-            <NavBar/>
             <div>
                 <div>
                     {isLoginScreen ? (
--- frontend/src/pages/profile_page.jsx
+++ frontend/src/pages/profile_page.jsx
@@ -51,7 +51,6 @@ export default function ProfilePage() {
   }
   return (
     <>
-      <NavBar />
       <main className="profile-page">
         <UserProfile
           user={userProfile}
