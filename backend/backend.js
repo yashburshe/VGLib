1740713f427a69f012628d418cecf956bacd5a66
Yash Burshe <bursheyash2@gmail.com>
2026-03-19 15:59:00 -0400

    Fix express 5 syntax

--- backend/backend.js
+++ backend/backend.js
@@ -37,7 +37,7 @@ app.use("/api/games", gamesRouter);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
