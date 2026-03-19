43931bb1539ce58aa6c926863f630d94c169a951
Ben Piperno <103087542+brpiperno@users.noreply.github.com>
2026-03-19 00:16:31 -0400

    refactored logic

--- frontend/src/js/game.js
+++ frontend/src/js/game.js
@@ -4,33 +4,10 @@ import { makeAuthReq } from "./frontEndUtils";
 
 
 export async function createGame(game) {
-    //TODO: confirm backend is complete!
-    console.error("Backend does not return any indication of success!")
-    const token = localStorage.getItem('token');
-    if (!token) {
-        console.warn("No authentication token found");
-        return null;
-    }
-    try {
-        const response = await fetch("/api/games", {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify({ game })
-        });
-        const data = await response.json();
-        if (!response.ok) {
-            console.error("Session error: ", data.message);
-            return [];
-        }
-        console.log("Game Created successfully: ", data);
-        return data;
-    } catch (error) {
-        console.error(error);
-        return [];
-    }
+    const data = await makeAuthReq('/api/games', 'POST', {game});
+    return data;
+}
+
 export async function getGame(gameID) {
     const data = await makeAuthReq(`/api/games/${gameID}`, 'GET');
     if (data) return data.game;
